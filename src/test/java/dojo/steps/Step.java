38631ecf12b1800ac33501597cf32b4f78ed0de9
sfauvel <sfauvel@sfauvel-XPS-15-9560>
2018-01-22 11:27:35 +0100

    Initial project

--- src/test/java/dojo/steps/Step.java
+++ src/test/java/dojo/steps/Step.java
@@ -1,27 +1,5 @@
 package dojo.steps;
 
-import cucumber.api.PendingException;
-import cucumber.api.java.fr.Alors;
-import cucumber.api.java.fr.Etantdonné;
-import cucumber.api.java.fr.Quand;
-
 public class Step {
-	@Etantdonné("^un état initial$")
-	public void un_état_initial() throws Exception {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@Quand("^je fais quelque chose$")
-	public void je_fais_quelque_chose() throws Exception {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@Alors("^je devrais obtenir un résultat$")
-	public void je_devrais_obtenir_un_résultat() throws Exception {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
 
 }
